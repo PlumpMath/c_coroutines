df9ef526d23cbd7f7004293ea00ebed52853ab6f
Brian Lee <brian.kihoon.lee@gmail.com>
2016-12-23 23:50:58 -0500

    Clean up primegen.c code

--- primegen.c
+++ primegen.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <ucontext.h>
 
-#define MAX_PRIMES 1000
-
 int temp = 0;
 ucontext_t main_context, *gen_context, *return_context;
 
@@ -36,28 +34,25 @@ void check_divisibility(
     }
 }
 
+ucontext_t *initialize_context() {
+    ucontext_t *new_context = malloc(sizeof (ucontext_t));
+    getcontext(new_context);
+    new_context->uc_stack.ss_sp = malloc(sizeof (char) * SIGSTKSZ);
+    new_context->uc_stack.ss_size = sizeof (char) * SIGSTKSZ;
+    return new_context;
+}
+
 int main(int argc, char** argv) {
-    return_context = malloc(sizeof (ucontext_t));
-    getcontext(return_context);
-    return_context->uc_stack.ss_sp = malloc(sizeof (char) * SIGSTKSZ);
-    return_context->uc_stack.ss_size = sizeof (char) * SIGSTKSZ;
+    return_context = initialize_context();
+    gen_context = initialize_context();
 
-
-    gen_context = malloc(sizeof (ucontext_t));
-    getcontext(gen_context);
-    gen_context->uc_stack.ss_sp = malloc(sizeof (char) * SIGSTKSZ);
-    gen_context->uc_stack.ss_size = sizeof (char) * SIGSTKSZ;
     makecontext(gen_context, (void (*) (void)) generator, 4, return_context, gen_context, &temp, 2);
-
 
     while (1) {
         swapcontext(return_context, gen_context);
         printf("%d\n", temp);
 
-        ucontext_t *temp_context = malloc(sizeof (ucontext_t));
-        getcontext(temp_context);
-        temp_context->uc_stack.ss_sp = malloc(sizeof (char) * SIGSTKSZ);
-        temp_context->uc_stack.ss_size = sizeof (char) * SIGSTKSZ;
+        ucontext_t *temp_context = initialize_context();
         makecontext(return_context, (void (*) (void)) check_divisibility, 5, temp_context, return_context, gen_context, &temp, temp);
 
         gen_context = return_context;
